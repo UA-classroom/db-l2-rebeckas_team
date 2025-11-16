489373129a80a1943e9e201def0112c66ef0fe03
Rebecka Larsson <familjenskung@gmail.com>
2025-11-16 16:22:03 +0100

    Create first scheema

--- schemas.py
+++ schemas.py
@@ -1,3 +1,42 @@
 # Add Pydantic schemas here that you'll use in your routes / endpoints
 # Pydantic schemas are used to validate data that you receive, or to make sure that whatever data
 # you send back to the client follows a certain structure
+
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+
+class BusinessCreate(BaseModel):
+    """
+    Used when a client creates a business (POST).
+    Mandatory to send owner_id(int) and name(string max 30 characters)
+    Optional: main_category_id(int), description(string), street_name(sting),
+    street_number(string), city(string), postal_code(string)
+    """
+    owner_id: int
+    main_category_id: Optional[int] = None
+    name: str
+    description: Optional[str] = None
+    street_name: Optional[str] = None
+    street_number: Optional[str] = None
+    city: Optional[str] = None
+    postal_code: Optional[str] = None
+
+
+class BusinessOut(BaseModel):
+    """
+    Used when we RETURN a business to the client (GET).
+    Includes id and created_at.
+    """
+    id: int
+    owner_id: int
+    main_category_id: Optional[int] = None
+    name: str
+    description: Optional[str] = None
+    street_name: Optional[str] = None
+    street_number: Optional[str] = None
+    city: Optional[str] = None
+    postal_code: Optional[str] = None
+    created_at: datetime
+
